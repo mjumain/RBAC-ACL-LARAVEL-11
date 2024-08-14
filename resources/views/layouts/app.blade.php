2159a5af1b82737e3d2d470aa69ba20c943f5590
mjumain <mjumain11@gmail.com>
2024-08-14 09:08:07 +0700

    update app name

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -25,7 +25,7 @@
             <a href="{{ url()->current() }}" class="brand-link">
                 <img src="{{ asset('') }}assets/dist/img/AdminLTELogo.png" alt="AdminLTE Logo"
                     class="brand-image img-circle elevation-3" style="opacity: .8">
-                <span class="brand-text font-weight-light">AdminLTE 3</span>
+                <span class="brand-text font-weight-light">{{ env('APP_NAME') }}</span>
             </a>
 
             @include('layouts.component.sidebar')
